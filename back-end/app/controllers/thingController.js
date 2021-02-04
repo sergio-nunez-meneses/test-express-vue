7a716af25c7afc577093260d76bf3b19d4b9e035
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-04 13:34:01 +0100

    back-end: added thing controller's base functions

---
+++ back-end/app/controllers/thingController.js
@@ -0,0 +1,30 @@
+const db = require('../models/index');
+const ash = require('express-async-handler');
+
+exports.create = ash(async function(req, res) {
+  //
+});
+
+exports.findAll = ash(async function(req, res) {
+  //
+});
+
+exports.findOne = ash(async function(req, res) {
+  //
+});
+
+exports.update = ash(async function(req, res) {
+  //
+});
+
+exports.delete = ash(async function(req, res) {
+  //
+});
+
+exports.deleteAll = ash(async function(req, res) {
+  //
+});
+
+exports.findAllPublished = ash(async function(req, res) {
+  //
+});
